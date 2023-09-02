9ed14ba9fc3a588186863ae2a2bd54c00907ba3f
yeonje_seo <yj.s@interiorteacher.com>
2023-09-02 16:48:51 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/yj/patpet/PatpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.patpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatpetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
