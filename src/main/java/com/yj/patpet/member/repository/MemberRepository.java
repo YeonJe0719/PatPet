9fb3400e5adf34703876e0f9fc34e93b8f3bb5f8
YeonJe0719 <yj.s@interiorteacher.com>
2023-09-05 21:36:22 +0900

    db 접속 설정 및 테스트 엔티티 생성

---
+++ src/main/java/com/yj/patpet/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.yj.patpet.member.repository;
+
+import com.yj.patpet.member.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
